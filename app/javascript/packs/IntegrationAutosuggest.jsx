333b8a809eea4fb7c46fbdf9f1475aa1aad42006
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-02-09 10:28:39 -0600

    Add help text

--- app/javascript/packs/IntegrationAutosuggest.jsx
+++ app/javascript/packs/IntegrationAutosuggest.jsx
@@ -152,7 +152,7 @@ class IntegrationAutosuggest extends React.Component {
         renderSuggestion={renderSuggestion}
         inputProps={{
           classes,
-          placeholder: 'Search a service area (HVAC) or vendor (ABC Contracting)',
+          placeholder: 'Type the letter \'C\' ',
           value: this.state.value,
           onChange: this.handleChange,
         }} />
