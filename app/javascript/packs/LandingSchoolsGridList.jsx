e5b0a8d2a050de5ed0eb1972ff02e1b83e8d2ef5
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-02-12 14:58:56 -0600

    Change gridlist to vertical

--- app/javascript/packs/LandingSchoolsGridList.jsx
+++ app/javascript/packs/LandingSchoolsGridList.jsx
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Grid from 'material-ui/Grid'
 import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
+import Subheader from 'material-ui/List/ListSubheader';
+import InfoIcon from 'material-ui-icons/Info';
+
 import IconButton from 'material-ui/IconButton';
 import StarBorderIcon from 'material-ui-icons/StarBorder';
 
@@ -15,51 +18,57 @@ const styles = theme => ({
     backgroundColor: theme.palette.background.paper,
   },
   gridList: {
-    flexWrap: 'nowrap',
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: 'translateZ(0)',
+    width: 500,
+    height: 450,
   },
-  title: {
-    color: theme.palette.primary.dark,
-  },
-  titleBar: {
-    background:
-      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
   },
 });
 
 
 const tileData = [
   {
+    img: "https://pbs.twimg.com/profile_images/1595191063/CCPCS-logo-icon-CMYK_WEB_400x400.gif",
+    title: 'Capital City PCS',
+    id: 8
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/1786302154/Twitter_Logo03_400x400.png",
+    title: 'DC PREP',
+    id: 4
+  },
+  {
     img: "https://pbs.twimg.com/profile_images/900102187933499392/vrbWtNB_.jpg",
     title: 'E.L. Haynes PCS',
     id: 1
   },
   {
-    img: "https://i.vimeocdn.com/portrait/8834034_300x300",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCVBYmU7Qq0GyDzY4p22IgFuvuwg-AnDi5ZP6SX6DAg7T9y5cI",
+    title: 'Friendship PCS',
+    id: 7
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/2499611872/ov32xj1wrupsyzrdmbzt.png",
+    title: 'IDEA PCS',
+    id: 5
+  },
+  {
+    img: "https://pbs.twimg.com/profile_images/762746251653816320/VjRsHYrC_400x400.jpg",
     title: 'KIPP DC',
     id: 2
   },
   {
-    img: "https://media.glassdoor.com/sqll/362024/paul-public-charter-school-squarelogo.png",
+    img: "https://pbs.twimg.com/profile_images/532975075054477312/uyY9cZTC_400x400.png",
     title: 'Paul PCS',
     id: 3
   },
   {
-    img: "http://www.dcprep.org/images/dcprep_logo.jpg",
-    title: 'DC PREP',
-    id: 4
-  },
-  {
-    img: "https://pbs.twimg.com/profile_images/2499611872/ov32xj1wrupsyzrdmbzt.png",
-    title: 'IDEA',
-    id: 5
-  },
-  {
-    img: "https://pbs.twimg.com/profile_images/900102187933499392/vrbWtNB_.jpg",
-    title: 'E.L. Haynes PCS',
+    img: "https://pbs.twimg.com/profile_images/378800000152200172/ce6a7a78db2c4a2ae6e74b6014da6bb9_400x400.jpeg",
+    title: 'Two Rivers PCS',
     id: 6
   },
+
 
 ];
 
@@ -69,35 +78,26 @@ class LandingSchoolsGridList extends React.Component {
 
     const { classes } = this.props;
 
-    return (
-      <Grid container
-            className={classes.root}
-            alignItems='flex-start'
-            direction= 'row'
-            justify= 'center'
-            spacing={24} >
-        <Grid item xs={12} >
-          <GridList className={classes.gridList} cols={2.5}>
-            {tileData.map(tile => (
-              <GridListTile key={tile.id} style={{ maxHeight: 'auto', width: 250 }}>
-                <img src={tile.img} alt={tile.title}  />
-                <GridListTileBar
-                  title={tile.title}
-                  classes={{
-                    root: classes.titleBar,
-                    title: classes.title,
-                  }}
-                  actionIcon={
-                    <IconButton>
-                      <StarBorderIcon className={classes.title} />
-                    </IconButton>
-                  } />
-              </GridListTile>
-            ))}
-          </GridList>
+      return (
+        <Grid container
+              alignItems='center'
+              direction= 'row'
+              justify= 'center'>
+          <div className={classes.root}>
+            <GridList cellHeight={180} className={classes.gridList}>
+              {tileData.map(tile => (
+                <GridListTile key={tile.img}>
+                  <img src={tile.img} alt={tile.title} />
+                  <a>
+                    <GridListTileBar
+                      title={tile.title} />
+                  </a>
+                </GridListTile>
+              ))}
+            </GridList>
+          </div>
         </Grid>
-      </Grid>
-    );
+  );
     }
 }
 
