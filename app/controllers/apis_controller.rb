ccb67cb859753b1ff13ba2e66c845116b67f1e11
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-02-15 10:31:56 -0700

    Add headers

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,9 +1,17 @@
 class ApisController < ApplicationController
 
+  before_filter :add_allow_credentials_headers
+
   def landing_search_data
     @categories = Category.all
     @data = { categories: @categories }
     render json: @data
+  end
+
+  def add_allow_credentials_headers
+    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Allow-Headers'] = 'accept, content-type'
   end
 
 end
