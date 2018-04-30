b93f1382f63252c10690d8797a6580d1fb7c78a1
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-04-30 14:09:51 -0500

    Add details and map to org show

---
+++ app/javascript/packs/Map.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const { compose, withProps, withState, withHandlers } = require("recompose");
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} = require("react-google-maps");
+
+const Map = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withState('zoom', 'onZoomChange', 10),
+  withHandlers(() => {
+    const refs = {
+      map: undefined,
+    }
+
+    return {
+      onMapMounted: () => ref => {
+        refs.map = ref
+      },
+      onZoomChanged: ({ onZoomChange }) => () => {
+        onZoomChange(refs.map.getZoom())
+      }
+    }
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultCenter={{ lat: 41.866669, lng: -87.605860 }}
+    zoom={props.zoom}
+    ref={props.onMapMounted}
+    onZoomChanged={props.onZoomChanged}
+  >
+    <Marker
+      position={{ lat: 41.866669, lng: -87.605860 }}
+      onClick={props.onToggleOpen}
+    >
+      <InfoWindow onCloseClick={props.onToggleOpen}>
+        <div>
+          {" "}
+          Noble Street PCS
+        </div>
+      </InfoWindow>
+    </Marker>
+  </GoogleMap>
+);
+
+export default (Map);
