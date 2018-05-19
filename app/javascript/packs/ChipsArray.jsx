20310189b407b91f47a211aef50b2f943cb638db
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-05-19 13:07:25 -0500

    Reformat through chips array

--- app/javascript/packs/ChipsArray.jsx
+++ app/javascript/packs/ChipsArray.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import Chip from 'material-ui/Chip';
-import Paper from 'material-ui/Paper';
-import Typography from 'material-ui/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 
 const styles = theme => ({
