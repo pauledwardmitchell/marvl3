683b1c5059560e28a08a8b2a1ebca3281c90d29a
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-06-07 14:03:50 -0500

    Remove old packages

--- app/javascript/packs/IntegrationAutosuggest.jsx
+++ app/javascript/packs/IntegrationAutosuggest.jsx
@@ -7,7 +7,7 @@ import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
 import { withStyles } from '@material-ui/core/styles';
-import Button from 'material-ui/Button';
+import Button from '@material-ui/core/Button';
 
 
 function renderInput(inputProps) {
