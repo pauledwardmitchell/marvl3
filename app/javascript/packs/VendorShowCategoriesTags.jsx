433112bc07966b3a0ac71933d55c387879fd6a60
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-06-07 13:52:43 -0500

    Load muiThemeProvider from new package

--- app/javascript/packs/VendorShowCategoriesTags.jsx
+++ app/javascript/packs/VendorShowCategoriesTags.jsx
@@ -6,7 +6,7 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Tooltip from '@material-ui/core/Tooltip';
 
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 
 
