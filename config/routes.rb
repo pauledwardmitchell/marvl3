69921581fc2269e407594c86ee322a90832aaec9
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-09-22 19:54:32 -0400

    Rename endpoint"

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   get '/landing_search_data', to:'apis#landing_search_data'
   get '/check_for_user', to:'apis#check_for_user'
+  get '/building_and_grounds', to: 'apis#building_and_grounds'
 
   root to: 'reviews#index'
 
