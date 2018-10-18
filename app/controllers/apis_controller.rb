5b7c832cb0548ffc3cab1c17502f76a54a6e73bd
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-10-18 14:02:27 -0400

    Add vendor id to endpoint

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -294,6 +294,7 @@ class ApisController < ApplicationController
         org_reviews.each do |review|
 
           review_data = { vendorName: Vendor.find(review.vendor_id).name,
+                          vendorId: review.vendor_id,
                           dateWritten: review.created_at.strftime("%m/%d/%Y"),
                           stars: review.rating_service,
                           review: review.review_content,
