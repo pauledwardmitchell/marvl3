654ff25ba9514d10913181561ecd20f5850b2538
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-10-24 14:10:44 -0400

    Edit review params allowed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -80,6 +80,6 @@ class ReviewsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
-      params.require(:review).permit(:user_id, :vendor_id, :review_content, :rating_service, :rating_quality, :anonymous)
+      params.require(:review).permit(:user_id, :vendor_id, :review_content, :review_private_content, :rating)
     end
 end
