d8c3c4a440ca8f101724ed2466781976b3287420
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-11-07 14:17:29 -0500

    Remove switch import

--- app/javascript/packs/AddProtipDialog.jsx
+++ app/javascript/packs/AddProtipDialog.jsx
@@ -6,7 +6,6 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import Switch from '@material-ui/core/Switch';
 import Typography from '@material-ui/core/Typography';
 import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
