adf171c1f58dcd5f817b6ea0788432294ec9e320
Paul Mitchell <pauledwardmitchell@gmail.com>
2019-03-14 13:45:00 -0400

    Refactor SignupPage

--- app/javascript/packs/TermsAndConditions.jsx
+++ app/javascript/packs/TermsAndConditions.jsx
@@ -9,7 +9,7 @@ const styles = theme => ({
   root: {
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
-    paddingBottom: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
   },
 });
 
