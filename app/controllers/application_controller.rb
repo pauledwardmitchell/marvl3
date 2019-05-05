3d3f24114817238a8d9d626935ad237319e2ff15
Paul Mitchell <pauledwardmitchell@gmail.com>
2019-05-05 12:52:40 -0400

    Add default_url_options to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def default_url_options
+    if Rails.env.production?
+      {:host => "schools.marvl.org"}
+    else
+      {}
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
