3dccb9372a7f551460a1bd12d9e20fb112673e6e
Paul Mitchell <pauledwardmitchell@gmail.com>
2019-10-28 09:46:49 -0600

    Add simple tooltip star

---
+++ app/javascript/packs/CategoryShowPreferredVendorBadge.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Tooltip from '@material-ui/core/Tooltip';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+
+const styles = theme => ({
+  fab: {
+    marginLeft: 10
+
+  },
+});
+
+function CategoryShowPreferredVendorBadge(props) {
+  const { classes } = props;
+
+  return (
+    <span>
+      <Tooltip title="CPA Preferred Vendor">
+        <StarBorderIcon className={classes.fab}/>
+      </Tooltip>
+    </span>
+  );
+}
+
+CategoryShowPreferredVendorBadge.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CategoryShowPreferredVendorBadge);
